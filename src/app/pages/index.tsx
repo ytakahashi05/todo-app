15d839842e8672efcd858684023066d8265af13c
ytakahashi <ytakahashi@USERnoMacBook-Pro.local>
2020-01-15 00:23:04 +0900

    we can deploy

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,7 +1,30 @@
-import App from '../components/App'
+import App from '../components/App';
+import * as React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  text-align: center;
+`;
+
+const Submit = styled.div`
+  padding: 10px;
+  border: solid 0.5px #DDD;
+  width: 300px;
+  height: 50px;
+  margin: auto;
+  text-align: center;
+`;
+
 
 export default () => (
-  <App>
-    <p>Index Page</p>
-  </App>
-)
+  <>
+    <Title>ToDos</Title>
+    <Submit>
+      <input name="submit todo"></input>
+      <button>提出</button>
+    </Submit>
+    <App>
+      <p>Index Page</p>
+    </App>
+  </>
+);
